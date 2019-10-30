88a417313c339e711e55c113ba2422a48a8a4b67
Daniel <herrnichols@gmail.com>
2019-10-30 16:54:49 -0400

    initial commit

--- src/app.js
+++ src/app.js
@@ -4,18 +4,20 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-
 const app = express();
-
 const morganOption = NODE_ENV === 'production';
+const bookmarksRouter = require('./bookmarks-router')
+const validateBearerToken = require('./validation')
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+app.use(validateBearerToken)
 
-app.get('/', (req, res) => {
-  res.send('Hallo, Textbaustein!');
-});
+
+app.use(bookmarksRouter)
+
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
---
+++ src/store.js
@@ -0,0 +1,25 @@
+const bookmarks = [
+	 {
+			id: '1',
+			title: 'reddit',
+			url: 'http://www.reddit.com',
+			description: 'The front page of the internet',
+			rating: 3
+	 },
+	 {
+			id: '2', 
+			title: 'Facebook',
+			url: 'http://www.facebook.com',
+			description: 'A place for 30 somethings', 
+			rating: 2
+	 }, 
+	 {
+			id: '3',
+			title: 'Google', 
+			url: 'http://www.google.com', 
+			description: 'The only way to search', 
+			rating: 4
+	 }
+]
+
+module.exports = bookmarks
